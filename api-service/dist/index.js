8a0a007fef273df12ddd6c2430fe9c8c0a461266
Vidhu Popli <vidhu278@gmail.com>
2024-10-18 15:36:24 +0530

    initial commit

---
+++ api-service/dist/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+const serverPort = process.env.SERVER_PORT;
+const app = express();
+app.get("/", (req, res, next) => {
+    res.send("Jai Gurudev | Narayan Narayan");
+});
+app.listen(serverPort, () => {
+    console.log("started server @ port: ", serverPort);
+});
